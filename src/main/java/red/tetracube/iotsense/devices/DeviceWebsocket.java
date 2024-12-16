6b079c25682e8c0eb96ed361d0ded39a4e2fe1d5
Davies <davide.dcube@gmail.com>
2024-12-16 23:12:25 +0100

    🐋 Fix port number, fix devices websocket endpoint

--- src/main/java/red/tetracube/iotsense/devices/DeviceWebsocket.java
+++ src/main/java/red/tetracube/iotsense/devices/DeviceWebsocket.java
@@ -11,7 +11,7 @@ import red.tetracube.iotsense.devices.payloads.api.DevicePayload;
 import java.util.Optional;
 import java.util.UUID;
 
-@WebSocket(path = "/devices")
+@WebSocket(path = "/devices/ws")
 public class DeviceWebsocket {
 
     @Inject
